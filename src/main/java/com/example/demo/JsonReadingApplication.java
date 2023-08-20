0aeed61d084f5f25cb97dbd7e9e864f0b96bc7cd
Deepak Garg <90500037+Deepakgarg2309@users.noreply.github.com>
2023-08-20 17:26:29 +0530

    Update JsonReadingApplication.java

--- src/main/java/com/example/demo/JsonReadingApplication.java
+++ src/main/java/com/example/demo/JsonReadingApplication.java
@@ -1,9 +1,13 @@
 
 package com.example.demo;
 
+import com.example.demo.model.Person;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,67 +21,35 @@ import java.util.Map;
 
 @SpringBootApplication
 public class JsonReadingApplication {
-	
-	 public static List<Map<String, Object>> parseJson(String jsonString) {
-        List<Map<String, Object>> result = new ArrayList<>();
+        
 
-        // Remove any leading/trailing whitespace and brackets [ ]
-        jsonString = jsonString.trim();
-        if (jsonString.startsWith("[")) {
-            jsonString = jsonString.substring(1);
-        }
-        if (jsonString.endsWith("]")) {
-            jsonString = jsonString.substring(0, jsonString.length() - 1);
-        }
+	public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person p = new Person();
 
-        String[] items = jsonString.split("\\},\\{");
-        for (String item : items) {
-            Map<String, Object> objMap = new LinkedHashMap<>();
+        p.setAge(25);
+        p.setName("Ankur");
+        p.setSalary(1900.90);
+        Person p1 = new Person();
 
-            // Remove leading/trailing brackets { }
-            item = item.trim();
-            if (item.startsWith("{")) {
-                item = item.substring(1);
-            }
-            if (item.endsWith("}")) {
-                item = item.substring(0, item.length() - 1);
-            }
+        p1.setAge(30);
+        p1.setName("Anuj");
+        p1.setSalary(3999.90);
 
-            String[] keyValuePairs = item.split(",");
-            for (String pair : keyValuePairs) {
-                String[] keyValue = pair.split(":");
-                String key = keyValue[0].trim().replaceAll("\"", "");
-                String value = keyValue[1].trim();
+        List<Person> persons= new ArrayList<>();
+        persons.add(p);
+        persons.add(p1);
+        String jsonString2 = objectMapper.writeValueAsString(persons);
+        System.out.println(jsonString2);
+        List<Person> person = objectMapper.readValue(jsonString2, new TypeReference<List<Person>>() {
+        });
+        System.out.println(person.get(0).getName());
+        System.out.println(person.get(0).getAge());
+        System.out.println(person.get(0).getSalary());
+        System.out.println(person.get(1).getName());
+        System.out.println(person.get(1).getAge());
+        System.out.println(person.get(1).getSalary());
 
-                if (value.startsWith("\"") && value.endsWith("\"")) {
-                    // String value
-                    value = value.replaceAll("\"", "");
-                    objMap.put(key, value);
-                } else if (value.contains(".")) {
-                    // Floating-point number
-                    ArbitraryDecimalPrecisionNumber decimalValue = new ArbitraryDecimalPrecisionNumber(value);
-                    objMap.put(key, decimalValue);
-                } else {
-                    // Integer number
-                    //System.out.println(value);
-                    ArbitraryPrecisionNumber  longValue= new ArbitraryPrecisionNumber(value);
-                    //System.out.println(longValue);
-                    objMap.put(key, longValue);
-                }
-            }
-
-            result.add(objMap);
-        }
-
-        return result;
-    }
-	public static void main(String[] args) {
-		String jsonString = "[{\"name\":\"Ankur\",\"age\":27,\"salary\":10657657647564647600.50654647547545454345344365746},{\"name\":\"Anuj\",\"age\":25,\"salary\":2000.75}]";
-
-        List<Map<String, Object>> resultList = parseJson(jsonString);
-        for (Map<String, Object> objMap : resultList) {
-            System.out.println(objMap);
-        }
 	}
 
 }
